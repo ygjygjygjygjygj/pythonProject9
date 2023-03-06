edbeba06b168de36537fdd1985f58db005942c86
13989 <13989@ygj>
2023-03-06 22:31:33 +0800

    ygj

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('3.1415926')
+    print_hi('3.16')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
